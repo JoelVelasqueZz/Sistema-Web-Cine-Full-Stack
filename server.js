58bb498187c8c0834489bc80994df9b9f7afeeb5
joel <velajoel2005@gmail.com>
2025-06-21 13:15:32 -0500

    Fix dist path in server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Servir archivos estáticos desde dist/
-app.use(express.static(path.join(__dirname, 'dist/proyecto-cine')));
+// Servir archivos estáticos desde src/dist/
+app.use(express.static(path.join(__dirname, 'src/dist/parkyfilms-frontend')));
 
-// Todas las rutas van al index.html (para Angular routing)
+// Todas las rutas van al index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/proyecto-cine/index.html'));
+  res.sendFile(path.join(__dirname, 'src/dist/parkyfilms-frontend/index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
