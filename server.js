9092265e4b84cff9f4ff0a1ddf87b315b0a491a3
joel <velajoel2005@gmail.com>
2025-06-21 13:20:13 -0500

    Fix path to match angular.json outputPath

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Servir archivos estáticos desde src/dist/
-app.use(express.static(path.join(__dirname, 'src/dist/parkyfilms-frontend')));
+// Servir archivos estáticos desde src/dist/proyecto-cine
+app.use(express.static(path.join(__dirname, 'src/dist/proyecto-cine')));
 
 // Todas las rutas van al index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/dist/parkyfilms-frontend/index.html'));
+  res.sendFile(path.join(__dirname, 'src/dist/proyecto-cine/index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
