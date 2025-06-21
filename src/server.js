75d7813c9807c13879681fda75a289c2e74b4406
joel <velajoel2005@gmail.com>
2025-06-21 14:13:06 -0500

    Fix server.js path to point to correct dist location

--- src/server.js
+++ src/server.js
@@ -4,12 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Servir archivos estáticos desde dist/proyecto-cine (misma carpeta)
-app.use(express.static(path.join(__dirname, 'dist/proyecto-cine')));
+// La ruta correcta es ../dist/proyecto-cine (no ./dist/proyecto-cine)
+app.use(express.static(path.join(__dirname, '../dist/proyecto-cine')));
 
-// Todas las rutas van al index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/proyecto-cine/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/proyecto-cine/index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
